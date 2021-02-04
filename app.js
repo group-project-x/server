0626710edbf817dbc0d9a72bc81cde02a1cee9e8
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2021-02-04 13:30:59 +0700

    login sudah selesai

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body
+  const access_token
+
+  res.status(200).json({ access_token})
+})
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })
